e9b3d1a0cc057971a0195cca2915cfb5456f1610
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-08-22 12:47:55 -0700

    Put card information into a js object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -12,11 +33,14 @@ function checkForMatch() {
 }
 
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
 	checkForMatch();
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
